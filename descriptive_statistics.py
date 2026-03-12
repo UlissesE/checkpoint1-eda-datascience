6d856cd1b60adcef091ca5b3071e86a63454f14e
henriguedes <guedesxx24@gmail.com>
2026-03-12 20:38:17 -0300

    statisticas

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -1,7 +1,14 @@
 import pandas as pd
 
-data = pd.read_csv("data/processed/dataset_clean.csv")
+df = pd.read_csv("data/processed/dataset_clean.csv")
 
-df = data[['ai_adoption_rate', 'revenue_growth_percent']].copy()
+
+
+df = data[['ai_adoption_rate', 'productivity_change_percent']].copy()
 pd.set_option('display.width', None)
-print(df.head(50))
+print(df.head(8))
+print("media : \n", df.mean())
+print("mediana :  \n", df.median())
+print("Moda: \n", df.mode())
+print("Variância amostral: \n", df.var(ddof=1))
+print("DP amostral: \n", df.std(ddof=1))
