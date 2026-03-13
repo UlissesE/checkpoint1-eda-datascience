236e834116e77abff57a0338e9dd0df0122213a3
Eduardo Ulisses <eduardoulisses2006@gmail.com>
2026-03-13 00:42:56 -0300

    double graphics

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -3,19 +3,38 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("data/processed/dataset_clean.csv")
 
-df = data[['ai_adoption_rate', 'productivity_change_percent']].copy()
-print(df.head(8))
-print(df.shape)
-print("media : \n", df.mean())
-print("mediana :  \n", df.median())
-print("Moda: \n", df.mode())
-print("Variância amostral: \n", df.var(ddof=1))
-print("DP amostral: \n", df.std(ddof=1))
 
-plt.hist(df['ai_adoption_rate'], bins="auto", alpha=0.7, label="AI Adoption")
-plt.hist(df['productivity_change_percent'], bins="auto", alpha=0.7,label="Produtividade")
-plt.title("Histograma: Adoção AI / Aumento Produtividade")
-plt.xlabel("%")
-plt.ylabel("frequência")
-plt.legend()
+df = data[['ai_adoption_rate', 'productivity_change_percent']].copy()
+
+print(
+    """
+    ==========================================
+    Estatística descritiva “com interpretação”
+    ==========================================
+    """
+)
+
+print(df.head(8))
+print("\n")
+print("\nMédia:\n", df.mean())
+print("\nMediana:\n", df.median())
+print("\nModa:\n", df.mode())
+print("\nVariância Amostral:\n", df.var(ddof=1))
+print("\nDP amostral:\n", df.std(ddof=1))
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+
+# Ai Adoption Rate Graphics
+axes[0].hist(df['ai_adoption_rate'], bins="auto", color="steelblue", alpha=0.7)
+axes[0].set_title("Histograma: AI Adoption Rate")
+axes[0].set_xlabel("%")
+axes[0].set_ylabel("Frequência")
+
+# Productivity Change Percent Graphics
+axes[1].hist(df['productivity_change_percent'], bins="auto", color="orange", alpha=0.7)
+axes[1].set_title("Histograma: Productivity Change")
+axes[1].set_xlabel("%")
+axes[1].set_ylabel("Frequência")
+
+plt.tight_layout()
 plt.show()
